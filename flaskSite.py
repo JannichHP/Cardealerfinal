e61632f4fe27d8066758e75848a051d356a43f67
Jannich Holm Pedersen <11043@student.eadania.dk>
2020-11-27 19:16:27 +0100

    update

--- flaskSite.py
+++ flaskSite.py
@@ -1,6 +1,31 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+app = Flask (__name__)
+app.config ['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///Dyrox.mysql.pythonanywhere-services.com'
+app.config['SECRET_KEY'] = "random string"
+
+
+db = SQLAlchemy(app)
+
+class usersignin(db.Model):
+   id = db.Column('user_id', db.Integer, primary_key = True)
+   Navn = db.Column(db.String(50))
+   Efternavn = db.Column(db.String(50))  
+   CVR = db.Column(db.String(8))
+   Email = db.Column(db.String(100))
+   Telefon = db.Column(db.String(8))
+   Adgangskode = db.Column(db.String(100))
+
+def __init__(self, Navn, Efternavn, CVR, Email, Telefon, Adgangskode):
+   self.Navn = Navn
+   self.Efternavn = Efternavn
+   self.CVR = CVR
+   self.Email = Email
+   self.Telefon = Telefon
+   self.Adgangskode = Adgangskode
+
+#usersignin.query.filter_by( = ’Toyota’).all()
 
 @app.route("/")
 def home():
@@ -21,7 +46,12 @@ def login_page():
 def home_page():
     return render_template("home.html")
 
-    
+
+@app.route("/profile")
+def profile():
+    return render_template("profile.html")
+
 if __name__ == "__main__":
+    db.create_all()
     app.run(debug=True)
 

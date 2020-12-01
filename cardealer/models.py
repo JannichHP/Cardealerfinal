f853e162bd2d279803e7b3897aba6d712d78781a
Jannich Holm Pedersen <11043@student.eadania.dk>
2020-12-01 13:56:03 +0100

    Update

--- cardealer/models.py
+++ cardealer/models.py
@@ -19,15 +19,5 @@ class Post(db.Model):
     content = db.Column(db.Text, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
-
-class Sell
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(100), nullable=False)
-    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
-    content = db.Column(db.Text, nullable=False)
-    engine = db.Column(db.Text, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-
-
     def __repr__(self):
         return f"User('{self.title}', '{self.date_posted}')"

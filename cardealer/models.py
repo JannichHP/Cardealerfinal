3b872155d804771eba12d7fc9006c3157bfa7f58
Jannich Holm Pedersen <11043@student.eadania.dk>
2020-12-12 20:40:36 +0100

    Update

---
+++ cardealer/models.py
@@ -0,0 +1,41 @@
+from datetime import datetime
+from cardealer import db, login_manager
+from flask_login import UserMixin
+from sqlalchemy import Model
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    company = db.Column(db.String(100), unique=True, nullable=False)
+    phone = db.Column(db.String(100), unique=True, nullable=False)
+    CVR = db.Column(db.String(100), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='avatar.png') 
+    carsdata = db.relationship('CarData', backref='author', lazy=True)
+
+
+class CarData(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    brand = db.Column(db.String(100), nullable=False)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.String(500), nullable=False)
+    model = db.Column(db.String(100), nullable=False)
+    engine = db.Column(db.String(100), nullable=False)
+    image_file = db.Column(db.String(20), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+#class CarData(db.Model):
+    #id = db.Column(db.Integer, primary_key=True)
+    #title = db.Column(db.String(100), nullable=False)
+    #date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    #content = db.Column(db.String(500), nullable=False)
+    #user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    #image_file = db.Column(db.String(20), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.title}', '{self.date_posted}')"
